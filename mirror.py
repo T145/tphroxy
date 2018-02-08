010d5c7049ba2fae214973da44a0aa3d3c6bd562
T145 <gnosoman@gmail.com>
2018-02-08 16:24:57 -0500

    Rewriting the app from scratch

--- mirror.py
+++ mirror.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright 2008-2014 Brett Slatkin, 2018 Taylor Shuler
+# Copyright 2018 Taylor Shuler
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,129 +15,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__author__ = 'Brett Slatkin (bslatkin@gmail.com)'
-__modify__ = 'Taylor Shuler (gnosoman@gmail.com)'
+__author__ = 'Taylor Shuler (gnosoman@gmail.com)'
 
 import logging
-import urllib
+import os
+
+from google.appengine.api import app_identity, urlfetch
+
+from jinja2 import Environment, FileSystemLoader
+
+from urllib import unquote
+from urlparse import urlparse
+
+# Google imports webapp2 last, so I guess we will too
 import webapp2
 
-from google.appengine.api import urlfetch
-from google.appengine.ext.webapp import template
+env = Environment(
+    loader = FileSystemLoader(os.path.dirname(__file__))
+)
 
-from transform_content import transform_content
-
-DEBUG = True
-EXPIRATION_DELTA_SECONDS = 3600
-
-IGNORE_HEADERS = frozenset([  # Ignore hop-by-hop headers
-    'set-cookie',
-    'expires',
-    'cache-control',
-    'connection',
-    'keep-alive',
-    'proxy-authenticate',
-    'proxy-authorization',
-    'te',
-    'trailers',
-    'transfer-encoding',
-    'upgrade'
-])
-
-TRANSFORMED_CONTENT_TYPES = frozenset(['text/html', 'text/css'])
-
-
-class MirroredContent(object):
-
-    def __init__(self, original_address, translated_address, status, headers, data, base_url):
-        self.original_address = original_address
-        self.translated_address = translated_address
-        self.status = status
-        self.headers = headers
-        self.data = data
-        self.base_url = base_url
-
-    @staticmethod
-    def fetch(base_url, translated_address, mirrored_url):
-
-        logging.debug("Fetching '%s'", mirrored_url)
-        try:
-            response = urlfetch.fetch(mirrored_url)
-        except Exception as err:
-            logging.exception("Could not fetch URL: %s (%s)" % (mirrored_url, err))
-            return None
-
-        adjusted_headers = {}
-        for (key, value) in response.headers.iteritems():
-            adjusted_key = key.lower()
-            if adjusted_key not in IGNORE_HEADERS:
-                adjusted_headers[adjusted_key] = value
-
-        content = response.content
-        page_content_type = adjusted_headers.get('content-type', '')
-
-        for content_type in TRANSFORMED_CONTENT_TYPES:
-            # startswith() because there could be a 'charset=UTF-8' in the header.
-            if page_content_type.startswith(content_type):
-                content = transform_content(base_url, mirrored_url, content)
-                break
-
-        new_content = MirroredContent(
-            base_url = base_url,
-            original_address = mirrored_url,
-            translated_address = translated_address,
-            status = response.status_code,
-            headers = adjusted_headers,
-            data = content)
-
-        return new_content
-
-
-class BaseHandler(webapp2.RequestHandler):
-
-    def get_relative_url(self):
-        slash = self.request.url.find(r"/", len(self.request.scheme + '://'))
-        if slash == -1:
-            return r"/"
-        logging.debug('@BaseHandler | slash: %s', slash)
-        logging.debug('@BaseHandler | request.scheme: %s', self.request.scheme)
-        logging.debug('@BaseHandler | request.url: %s', self.request.url)
-        logging.debug('@BaseHandler | get_relative_url: %s', self.request.url[slash:])
-        return self.request.url[slash:]
+class BasePage(webapp2.RequestHandler):
 
     def is_recursive_request(self):
-        if 'AppEngine-Google' in self.request.headers.get('User-Agent', ''):
-            logging.warning('Ignoring recursive request by user-agent=%r; ignoring')
-            self.error(404)
-            return True
-        return False
+        user_agent = self.request.headers.get('User-Agent', None)
+        is_self = 'AppEngine-Google' in user_agent
 
+        if is_self:
+            logging.warning('@MainPage | RECURSIVE REQUEST - user_agent: %s', user_agent)
 
-class HomeHandler(BaseHandler):
+        return is_self
+
+    def strip_scheme(self, url):
+        parsed_url = urlparse(url)
+        scheme = "%s://" % parsed_url.scheme
+        return parsed_url.geturl().replace(scheme, '', 1)
+
+    def get_secure_url(self):
+        if self.request.scheme.startswith('https'):
+            return self.request.url
+        return None
+
+class MainPage(BasePage):
 
     def get(self):
         if self.is_recursive_request():
             return
 
-        # Handle the input form to redirect the user to a relative url
-        form_url = self.request.get('url')
-        if form_url:  # Accept URLs that still have a leading 'http(s)://'
-            inputted_url = urllib.unquote(form_url)
-            if inputted_url.startswith('http'):
-                inputted_url = inputted_url.replace('://', '_', 1)
-            else:
-                inputted_url = 'http_' + inputted_url
-            return self.redirect(r"/" + inputted_url)
+        input_url = self.request.get('url')
 
-        # Do this dictionary construction here, to decouple presentation from how we store data.
-        secure_url = None
-        if self.request.scheme == 'http':
-            secure_url = 'https://%s%s' % (self.request.host, self.request.path_qs)
-        context = {'secure_url': secure_url}
-        self.response.out.write(template.render('main.html', context))
+        if input_url:
+            input_url = self.strip_scheme(unquote(input_url))
+            return self.redirect(r'/' + input_url)
 
+        self.response.out.write(env.get_template("main.html").render({'secure_url': self.get_secure_url()}))
 
-class MirrorHandler(BaseHandler):
+class MirrorPage(BasePage):
 
     def get(self, base_url):
         if self.is_recursive_request():
@@ -145,25 +77,13 @@ class MirrorHandler(BaseHandler):
 
         assert base_url
 
-        # Log the user-agent and referrer, to see who is linking to us.
-        logging.debug('@MirrorHandler | user_agent: "%s"; referer: "%s"', self.request.user_agent, self.request.referer)
-        logging.debug('@MirrorHandler | base_url: "%s"; request.url: "%s"', base_url, self.request.url)
+        logging.debug('@MirrorPage | base_url: %s', base_url)
+        logging.debug('@MirrorPage | user_agent: %s; referer: %s', self.request.user_agent, self.request.referer)
 
-        translated_address = self.get_relative_url()[1:]  # remove leading /
-        if translated_address.startswith('http'):
-            (scheme, url) = translated_address.split('_', 1)
-            mirrored_url = '%s://%s' % (scheme, url)
-        else:
-            mirrored_url = 'http://%s' % translated_address
+        identity = app_identity.get_default_version_hostname();
+        mirror_url = identity + '/' + base_url
 
-        content = MirroredContent.fetch(base_url, translated_address, mirrored_url)
-
-        for (key, value) in content.headers.iteritems():
-            self.response.headers[key] = value
-        if not DEBUG:
-            self.response.headers['cache-control'] = 'max-age=%d' % EXPIRATION_DELTA_SECONDS
-
-        self.response.out.write(content.data)
+        logging.debug('@MirrorPage | mirror_url: %s', mirror_url)
 
 
-app = webapp2.WSGIApplication([(r"/", HomeHandler), (r"/([^/]+).*", MirrorHandler)], debug=DEBUG)
+app = webapp2.WSGIApplication([(r'/', MainPage), (r"/([^/]+).*", MirrorPage)], debug = True)
